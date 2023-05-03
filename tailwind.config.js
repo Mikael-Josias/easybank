8c5fe3072bcbb07c5a47f714aa0abd6623ec043c
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-03 18:20:34 -0300

    style: add images and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         'dark-blue': 'hsl(233, 26%, 24%)',
         'bright-cyan': 'hsl(192, 70%, 51%)',
         'dark-gray': 'hsl(233, 8%, 62%)',
+        'light-gray': 'hsl(0, 0%, 98%)',
       },
       spacing: {
         'anchor-2': 'calc(100% - 4px)',
