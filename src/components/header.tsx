af18ce692e70b4705642586b0a5cf5595c4d8a0c
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-04 18:46:56 -0300

    fix: remove header nav on small screens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,16 +9,16 @@ export default function Header() {
   const [openMenu, setOpenMenu] = useState(false);
   return (
     <header className="w-full h-20 fixed top-0 bg-white z-10">
-      <div className="flex items-center justify-between lg:w-full xl:w-[1200px] px-10 xl:px-0 m-auto h-full">
+      <div className="flex items-center justify-between w-full xl:w-[1200px] px-10 xl:px-0 m-auto h-full">
         <Image src={Logo} alt="Easybank logo"/>
-        <nav className="flex h-full bg-gradient-to-r from-lime-green to-bright-cyan collapse lg:visible">
+        <nav className="lg:flex h-full bg-gradient-to-r from-lime-green to-bright-cyan hidden lg:visible">
           <a href="#Home" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Home</a>
           <a href="#About" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">About</a>
           <a href="#Contact" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Contact</a>
           <a href="#Blog" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Blog</a>
           <a href="#Careers" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Careers</a>
         </nav>
-        <RequestInviteButton collapse={'collapse lg:visible'}/>
+        <RequestInviteButton collapse={'hidden lg:block'}/>
         <Image src={openMenu? closeMenuIcon : MenuIcon} alt="Abrir menu" className="lg:hidden cursor-pointer" onClick={() => setOpenMenu(!openMenu)}/>
       </div>
 
