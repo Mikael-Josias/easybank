4df3d950a785573870def63be8c7ddfaed748a79
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-04 16:41:50 -0300

    style: mobile responsive header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,36 @@
 import Image from "next/image";
 import Logo from "../../public/logo.svg";
+import MenuIcon from "../../public/icon-hamburger.svg";
+import closeMenuIcon from "../../public/icon-close.svg";
 import RequestInviteButton from "./request-invite-button";
+import { useState } from "react";
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
   return (
-    <header className="flex items-center justify-between px-36 h-20 fixed w-full top-0 bg-white z-10">
-      <Image src={Logo} alt="Easybank logo"/>
-      <nav className="flex h-full bg-gradient-to-r from-lime-green to-bright-cyan">
-        <a href="#Home" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Home</a>
-        <a href="#About" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">About</a>
-        <a href="#Contact" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Contact</a>
-        <a href="#Blog" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Blog</a>
-        <a href="#Careers" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Careers</a>
-      </nav>
-      <RequestInviteButton/>
+    <header className="w-full h-20 fixed top-0 bg-white z-10">
+      <div className="flex items-center justify-between lg:w-full xl:w-[1200px] px-10 xl:px-0 m-auto h-full">
+        <Image src={Logo} alt="Easybank logo"/>
+        <nav className="flex h-full bg-gradient-to-r from-lime-green to-bright-cyan collapse lg:visible">
+          <a href="#Home" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Home</a>
+          <a href="#About" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">About</a>
+          <a href="#Contact" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Contact</a>
+          <a href="#Blog" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Blog</a>
+          <a href="#Careers" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Careers</a>
+        </nav>
+        <RequestInviteButton collapse={'collapse lg:visible'}/>
+        <Image src={openMenu? closeMenuIcon : MenuIcon} alt="Abrir menu" className="lg:hidden cursor-pointer" onClick={() => setOpenMenu(!openMenu)}/>
+      </div>
+
+      {openMenu && <div className="lg:hidden w-full bg-gradient-to-b from-dark-blue/80 to-transparent absolute p-4 pb-24">
+        <nav className="bg-white rounded w-full overflow-hidden flex flex-col items-center gap-3 py-4">
+          <a href="#Home" className="flex px-3 hover:h-anchor-2 items-center bg-white text-black h-full">Home</a>
+          <a href="#About" className="flex px-3 hover:h-anchor-2 items-center bg-white text-black h-full">About</a>
+          <a href="#Contact" className="flex px-3 hover:h-anchor-2 items-center bg-white text-black h-full">Contact</a>
+          <a href="#Blog" className="flex px-3 hover:h-anchor-2 items-center bg-white text-black h-full">Blog</a>
+          <a href="#Careers" className="flex px-3 hover:h-anchor-2 items-center bg-white text-black h-full">Careers</a>
+        </nav>
+      </div>}
     </header>
   );
 }
--- src/components/request-invite-button.tsx
+++ src/components/request-invite-button.tsx
@@ -1,6 +1,9 @@
+interface RequestInviteButtonProps {
+  collapse?: string;
+}
 
-export default function RequestInviteButton() {
+export default function RequestInviteButton(props?: RequestInviteButtonProps) {
   return (
-    <button className="bg-gradient-to-r from-lime-green to-bright-cyan text-white rounded-full px-6 py-2">Request Invite</button>
+    <button className={`bg-gradient-to-r from-lime-green to-bright-cyan text-white rounded-full px-6 py-2 ${props?.collapse}`}>Request Invite</button>
   );
 }
