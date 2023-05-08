3a6c61007ac2cb96fd745bc3716b26d808f0829f
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-08 15:12:59 -0300

    style: change mobile menu icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Logo from "../../public/logo.svg";
-import MenuIcon from "../../public/icon-hamburger.svg";
-import closeMenuIcon from "../../public/icon-close.svg";
 import RequestInviteButton from "./request-invite-button";
 import { useState } from "react";
+import { List, X } from "@phosphor-icons/react";
 
 export default function Header() {
-  const [openMenu, setOpenMenu] = useState(false);
+  const [toggleOpened, setToggleOpened] = useState(false);
   return (
     <header className="w-full h-20 fixed top-0 bg-white z-10">
       <div className="flex items-center justify-between w-full xl:w-[1200px] px-10 xl:px-0 m-auto h-full">
@@ -19,10 +18,12 @@ export default function Header() {
           <a href="#Careers" className="flex px-3 hover:h-anchor-2 items-center bg-white text-dark-gray h-full">Careers</a>
         </nav>
         <RequestInviteButton collapse={'hidden lg:block'}/>
-        <Image src={openMenu? closeMenuIcon : MenuIcon} alt="Abrir menu" className="lg:hidden cursor-pointer" onClick={() => setOpenMenu(!openMenu)}/>
+        <button className="p-2 rounded-full lg:hidden" onClick={() => setToggleOpened(!toggleOpened)}>
+          {toggleOpened? <X /> : <List />}
+        </button>
       </div>
 
-      {openMenu && <div className="lg:hidden w-full bg-gradient-to-b from-dark-blue/80 to-transparent absolute p-4 pb-24">
+      {toggleOpened && <div className="lg:hidden w-full bg-gradient-to-b from-dark-blue/80 to-transparent absolute p-4 pb-24">
         <nav className="bg-white rounded w-full overflow-hidden flex flex-col items-center gap-3 py-4">
           <a href="#Home" className="flex px-3 hover:h-anchor-2 items-center bg-white text-black h-full">Home</a>
           <a href="#About" className="flex px-3 hover:h-anchor-2 items-center bg-white text-black h-full">About</a>
@@ -34,3 +35,5 @@ export default function Header() {
     </header>
   );
 }
+
+    
